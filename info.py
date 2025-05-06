eae5cf8e221cfab3e83d3dd6bdf2876ecadd4fea
shakib6009 <shakibblogger2@gmail.com>
2025-05-06 09:39:16 +0800

    Update info.py

--- info.py
+++ info.py
@@ -1,132 +1,98 @@
-import re
-from os import environ,getenv
-from Script import script
+import telegram
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler
+from telegram.ext.filters import BaseFilter
+import logging
+import tmdbsimple as tmdb
 
-id_pattern = re.compile(r'^.\d+$')
-def is_enabled(value, default):
-    if value.lower() in ["true", "yes", "1", "enable", "y"]:
-        return True
-    elif value.lower() in ["false", "no", "0", "disable", "n"]:
-        return False
+# Enable logging
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+logger = logging.getLogger(__name__)
+
+# TMDB API Key
+tmdb.API_KEY = '6db3367018a3783ce9fccff017a9fb12'
+
+# Telegram Bot Token
+TELEGRAM_TOKEN = "7535439008:AAFYXGLjbanDwTAatoBg3ILPQUcqgKVt2MM"
+
+BASE_URL = "https://movie.banglafilx.com/watch/"
+
+class SearchFilter(BaseFilter):
+    def filter(self, message):
+        return bool(message.text and message.text.startswith("#search") and len(message.text.split()) > 1)
+
+search_filter = SearchFilter()
+
+async def movie(update, context):
+    if update.message and update.message.text:
+        if update.message.text.startswith("#search"):
+            try:
+                movie_name = update.message.text.split(' ', 1)[1]
+                logger.info(f"Movie name before replace: {movie_name}")
+                movie_name_hyphenated = movie_name.replace(" ", "-")
+                logger.info(f"Movie name after replace: {movie_name_hyphenated}")
+
+                # TMDB থেকে মুভির তথ্য সংগ্রহ করুন
+                search = tmdb.Search()
+                response = search.movie(query=movie_name)
+
+                if search.results:
+                    movie = search.results[0]
+                    movie_id = movie['id']
+                    movie_title = movie['title']
+                    movie_rating = movie['vote_average']
+                    movie_poster_path = movie['poster_path']
+                    movie_poster_url = None
+                    if movie_poster_path:
+                        movie_poster_url = f"https://image.tmdb.org/t/p/w500{movie_poster_path}"
+
+                    movie_link = BASE_URL + movie_name_hyphenated + ".html"
+                    message_text = f"🎬 Title: {movie_title}\n⭐ Rating: {movie_rating}\n🎭 Genres: {', '.join([genre['name'] for genre in movie['genres']]) if 'genres' in movie else 'N/A'}\n🗓️ Release Date: {movie['release_date'] if 'release_date' in movie else 'N/A'}\n{movie['overview'] if 'overview' in movie else 'N/A'}\nPlay and Download full movie\n\n{movie_link}\n\n🔥 Hi users thanks for join\n🌟 উপরের লিংক থেকে আপনার মুভিটি প্লে বা ডাউনলোড করুন। আরো আপডেট পেতে আমাদের টেলিগ্রাম গুরুপ এ join হোন, লিংক,, https://t.me/banglafilx2025\nযদি সঠিক মুভি/সিরিজ না পেয়ে থাকেন, তবে আবার 🎁 সঠিক নাম লিখে সার্চ দিন।\n🔎 যেমনঃ #search borbaad"
+                    if movie_poster_url:
+                        await context.bot.send_photo(chat_id=update.effective_chat.id, photo=movie_poster_url, caption=message_text)
+                    else:
+                        await context.bot.send_message(chat_id=update.effective_chat.id, text=message_text)
+                else:
+                    await context.bot.send_message(chat_id=update.effective_chat.id, text="Movie not found on TMDB. Please check the movie name or try again later.")
+
+            except IndexError:
+                await context.bot.send_message(chat_id=update.effective_chat.id, text="Please provide a movie name.")
+        else:
+            return
     else:
-        return default
-#---------------------------------------------------------------
-#---------------------------------------------------------------         ,
-SESSION = environ.get('SESSION', 'Media_search')
-API_ID = int(environ.get('API_ID', '23733855'))
-API_HASH = environ.get('API_HASH', '541afa01a117d5e60880795b356da6e1')
-BOT_TOKEN = environ.get('BOT_TOKEN', '7535439008:AAFYXGLjbanDwTAatoBg3ILPQUcqgKVt2MM')
-#---------------------------------------------------------------
-#---------------------------------------------------------------
-ADMINS = [int(admin) if id_pattern.search(admin) else admin for admin in environ.get('ADMINS', '6441511288').split()]
-USERNAME = environ.get('USERNAME', "https://t.me/Shakib6009") # ADMIN USERNAME
-LOG_CHANNEL = int(environ.get('LOG_CHANNEL', '-1002399488732'))
-MOVIE_GROUP_LINK = environ.get('MOVIE_GROUP_LINK', 'https://t.me/banglafilx2025')
-CHANNELS = [int(ch) if id_pattern.search(ch) else ch for ch in environ.get('CHANNELS', '-1002344960542').split()]
-#---------------------------------------------------------------
-#---------------------------------------------------------------
-DATABASE_URI = environ.get('DATABASE_URI', "u320501260_Movie")
-DATABASE_NAME = environ.get('DATABASE_NAME', "u320501260_Movie")
-COLLECTION_NAME = environ.get('COLLECTION_NAME', 'website_link')
-#---------------------------------------------------------------
-#---------------------------------------------------------------
-#----------- There will be channel id add in all these ---------
-LOG_API_CHANNEL = int(environ.get('LOG_API_CHANNEL', '0'))  
-BIN_CHANNEL = int(environ.get('BIN_CHANNEL','0'))
-DELETE_CHANNELS = int(environ.get('DELETE_CHANNELS','0'))
-LOG_VR_CHANNEL = int(environ.get('LOG_VR_CHANNEL', '0'))
-auth_channel = environ.get('AUTH_CHANNEL', '')
-SUPPORT_GROUP = int(environ.get('SUPPORT_GROUP', '0'))
-request_channel = environ.get('REQUEST_CHANNEL', '0')
-MOVIE_UPDATE_CHANNEL = int(environ.get('MOVIE_UPDATE_CHANNEL', '0'))
-SUPPORT_CHAT = environ.get('SUPPORT_CHAT', 'https://t.me/') #Support group link ( make sure bot is admin )
-#---------------------------------------------------------------
-#---------------------------------------------------------------
-IS_VERIFY = is_enabled('IS_VERIFY', True)
-#---------------------------------------------------------------
-TUTORIAL = environ.get("TUTORIAL", "https://t.me/")
-VERIFY_IMG = environ.get("VERIFY_IMG", "https://graph.org/file/1669ab9af68eaa62c3ca4.jpg")
-SHORTENER_API = environ.get("SHORTENER_API", "3097623f852197a9ce40d1212aaa8bbf2803e799")
-SHORTENER_WEBSITE = environ.get("SHORTENER_WEBSITE", 'omegalinks.in')
-SHORTENER_API2 = environ.get("SHORTENER_API2", "3097623f852197a9ce40d1212aaa8bbf2803e799")
-SHORTENER_WEBSITE2 = environ.get("SHORTENER_WEBSITE2", 'omegalinks.in')
-SHORTENER_API3 = environ.get("SHORTENER_API3", "3097623f852197a9ce40d1212aaa8bbf2803e799")
-SHORTENER_WEBSITE3 = environ.get("SHORTENER_WEBSITE3", 'omegalinks.in')
-TWO_VERIFY_GAP = int(environ.get('TWO_VERIFY_GAP', "14400"))
-THREE_VERIFY_GAP = int(environ.get('THREE_VERIFY_GAP', "14400"))
-#---------------------------------------------------------------
-#---------------------------------------------------------------
-LANGUAGES = ["hindi", "english", "telugu", "tamil", "kannada", "malayalam", "bengali", "marathi", "gujarati", "punjabi"]
-QUALITIES = ["HdRip","web-dl" ,"bluray", "hdr", "fhd" , "240p", "360p", "480p", "540p", "720p", "960p", "1080p", "1440p", "2K", "2160p", "4k", "5K", "8K"]
-YEARS = [f'{i}' for i in range(2024 , 2002,-1 )]
-SEASONS = [f'season {i}'for i in range (1 , 23)]
-REF_PREMIUM = 30
-PREMIUM_POINT = 1500
-#---------------------------------------------------------------
-AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_channel) else None
-REQUEST_CHANNEL = int(request_channel) if request_channel and id_pattern.search(request_channel) else None
-#---------------------------------------------------------------
-#---------------------------------------------------------------
-#---------------------------------------------------------------
-START_IMG = (environ.get('START_IMG', 'https://i.ibb.co/qpxpGmC/image.jpg https://i.ibb.co/DQ35zLZ/image.jpg')).split()
-FORCESUB_IMG = environ.get('FORCESUB_IMG', 'https://i.ibb.co/ZNC1Hnb/ad3f2c88a8f2.jpg')
-REFER_PICS = (environ.get("REFER_PICS", "https://envs.sh/PSI.jpg")).split() 
-PAYPICS = (environ.get('PAYPICS', 'https://envs.sh/_kA.jpg')).split()
-SUBSCRIPTION = (environ.get('SUBSCRIPTION', 'https://graph.org/file/9f3f47c690bbcc67633c2.jpg'))
-REACTIONS = ["👀", "😱", "🔥", "😍", "🎉", "🥰", "😇", "⚡"]
-#---------------------------------------------------------------
-#---------------------------------------------------------------
-#---------------------------------------------------------------
-FILE_AUTO_DEL_TIMER = int(environ.get('FILE_AUTO_DEL_TIMER', '600'))
-AUTO_FILTER = is_enabled('AUTO_FILTER', True)
-IS_PM_SEARCH = is_enabled('IS_PM_SEARCH', False)
-PORT = environ.get('PORT', '5000')
-MAX_BTN = int(environ.get('MAX_BTN', '8'))
-AUTO_DELETE = is_enabled('AUTO_DELETE', True)
-DELETE_TIME = int(environ.get('DELETE_TIME', 1200))
-IMDB = is_enabled('IMDB', False)
-FILE_CAPTION = environ.get('FILE_CAPTION', f'{script.FILE_CAPTION}')
-IMDB_TEMPLATE = environ.get('IMDB_TEMPLATE', f'{script.IMDB_TEMPLATE_TXT}')
-LONG_IMDB_DESCRIPTION = is_enabled('LONG_IMDB_DESCRIPTION', False)
-PROTECT_CONTENT = is_enabled('PROTECT_CONTENT', False)
-SPELL_CHECK = is_enabled('SPELL_CHECK', True)
-LINK_MODE = is_enabled('LINK_MODE', True)
+        logger.warning("Received a message without text")
+        return
 
-#---------------------------------------------------------------
-#---------------------------------------------------------------
-#---------------------------------------------------------------
-STREAM_MODE = bool(environ.get('STREAM_MODE', True)) # Set True or Flase
-# Online Stream and Download
+async def start(update, context):
+    await context.bot.send_message(chat_id=update.effective_chat.id, text="I'm a movie bot, send me a movie name to find the link!")
 
-MULTI_CLIENT = False
-SLEEP_THRESHOLD = int(environ.get('SLEEP_THRESHOLD', '60'))
-PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-if 'DYNO' in environ:
-    ON_HEROKU = True
-else:
-    ON_HEROKU = False
-URL = environ.get("FQDN", "")
+async def photo_handler(update: telegram.Update, context: telegram.ext.CallbackContext):
+    logger.info("photo_handler called")
+    if update.message.photo:
+        logger.info("photo detected")
+        message_text = "Hello users. Thanks for sending the image. We will search and update you as soon as possible."
+        await context.bot.send_message(chat_id=update.effective_chat.id, text=message_text)
+    else:
+        logger.info("no photo detected")
+        return
 
-#---------------------------------------------------------------
-#---------------------------------------------------------------
-SETTINGS = {
-            'spell_check': SPELL_CHECK,
-            'auto_filter': AUTO_FILTER,
-            'file_secure': PROTECT_CONTENT,
-            'auto_delete': AUTO_DELETE,
-            'template': IMDB_TEMPLATE,
-            'caption': FILE_CAPTION,
-            'tutorial': TUTORIAL,
-            'shortner': SHORTENER_WEBSITE,
-            'api': SHORTENER_API,
-            'shortner_two': SHORTENER_WEBSITE2,
-            'api_two': SHORTENER_API2,
-            'log': LOG_VR_CHANNEL,
-            'imdb': IMDB,
-            'link': LINK_MODE, 
-            'is_verify': IS_VERIFY, 
-            'verify_time': TWO_VERIFY_GAP,
-            'shortner_three': SHORTENER_WEBSITE3,
-            'api_three': SHORTENER_API3,
-            'third_verify_time': THREE_VERIFY_GAP
-}
+async def error(update, context):
+    logger.warning('Update "%s" caused error "%s"', update, context.error)
+
+def main():
+    try:
+        application = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
+
+        application.add_handler(CommandHandler("start", start))
+        application.add_handler(MessageHandler(search_filter, movie))
+        photo_handler_instance = MessageHandler(telegram.ext.filters.PHOTO, photo_handler)
+        application.add_handler(photo_handler_instance)
+        logger.info(f"Photo handler added: {photo_handler_instance}")
+
+        application.add_error_handler(error)
+
+        application.run_polling()
+    except Exception as e:
+        logger.error(f"An error occurred: {e}", exc_info=True)
+
+if __name__ == '__main__':
+    main()
